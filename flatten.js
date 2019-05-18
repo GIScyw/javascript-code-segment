ac84b5ff9c0a366954ce0f0172945be9a8c27f68
yawei.chen <yawei.chen@horizon.ai>
2019-05-18 16:48:01 +0800

    feat:增加扁平化函数

---
+++ flatten.js
@@ -0,0 +1,15 @@
+//数组的扁平化
+function flatten(arr) {
+    var result = [];
+    for (var i = 0, len = arr.length; i < len; i++) {
+        if (Array.isArray(arr[i])) {
+            result = result.concat(flatten(arr[i]))
+        }
+        else {
+            result.push(arr[i])
+        }
+    }
+    return result;
+}
+
+
